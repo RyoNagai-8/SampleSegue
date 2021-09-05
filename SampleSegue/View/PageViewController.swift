b86b945fbff83fb950f1b914e7c3428e2e11efb8
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-09-05 21:34:48 +0900

    PageViewController.swiftを編集

--- SampleSegue/View/PageViewController.swift
+++ SampleSegue/View/PageViewController.swift
@@ -14,7 +14,8 @@ class PageViewController: UIPageViewController {
         print("PageViewController")
 
         self.setViewControllers([getFirst()], direction: .forward, animated: true, completion: nil)
-        // Do any additional setup after loading the view.
+        self.dataSource = self
+        
     }
     
     func getFirst() -> FirstViewController {
@@ -24,6 +25,14 @@ class PageViewController: UIPageViewController {
     override func didReceiveMemoryWarning() {
            super.didReceiveMemoryWarning()
        }
+    
+    func getSecond() -> SecondViewController {
+        return storyboard!.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        }
+
+        func getThird() -> ThirdViewController {
+            return storyboard!.instantiateViewController(withIdentifier: "ThirdViewController") as! ThirdViewController
+        }
 
     /*
     // MARK: - Navigation
@@ -36,3 +45,33 @@ class PageViewController: UIPageViewController {
     */
 
 }
+
+extension PageViewController : UIPageViewControllerDataSource {
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if viewController.isKind(of: ThirdViewController.self) {
+                    // 3 -> 2
+                    return getSecond()
+        } else if viewController.isKind(of: SecondViewController.self) {
+                    // 2 -> 1
+                    return getFirst()
+                } else {
+                    // 1 -> end of the road
+                    return nil
+                }
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if viewController.isKind(of: FirstViewController.self) {
+                    // 1 -> 2
+                    return getSecond()
+        } else if viewController.isKind(of: SecondViewController.self) {
+                    // 2 -> 3
+                    return getThird()
+                } else {
+                    // 3 -> end of the road
+                    return nil
+                }
+    }
+}
+
